96f4de3c683880dd58e3ccce4aa355efeb7eec24
bhaveshbalendra <balendrabhavesh@gmail.com>
2025-02-06 14:37:11 +0530

    changes

--- frontend/src/constants/baseURL.ts
+++ frontend/src/constants/baseURL.ts
@@ -1,3 +1,4 @@
-const baseURL = "https://creaditsea-assignment-backend.onrender.com";
+const baseURL = "https://creaditsea-assignment-backend.onrender.com/api";
+// const baseURL = "http://localhost:8000/api";
 
 export default baseURL;
